031d5f5d2fb57a620d56acac9c6712deafdf6ebd
emacs45 <emec.acmi@gmail.com>
2024-05-07 21:08:49 +0200

    merged pr

--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------
 # This script searches xml files and parses particular xml tags
-# which contains several values and saves them to a txt file.
+# which contain several values and saves those to a txt file.
 #------------------------------------------------------------------
 import xml.etree.ElementTree as ET 
 import os
